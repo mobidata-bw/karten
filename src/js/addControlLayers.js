fae267d2eccdccd1f899e76de67684b1d16e86b2
Richard Forster <160242313+richardforsterNVBW@users.noreply.github.com>
2025-06-05 19:28:42 +0200

    Update addControlLayers.js

--- src/js/addControlLayers.js
+++ src/js/addControlLayers.js
@@ -4,7 +4,7 @@ import { legendCircle, legendLine, legendRectangle } from './controlLayers.js';
 export function addControlLayers(layers, directory, groupOrGroups) {
 
   return layers.map(layer => {
-    const { id, label, color, group, subGroup = '' } = layer; //layer.id, layer.label, ...
+    const { id, label, color, group, subGroup = '', type, exclusiveWithinGroup } = layer; //layer.id, layer.label, ...
 
     const roadworks = `<img class='legendIcon' src='${basePath}img/controlElements/verkehrszeichen.svg'></img>`;
 
@@ -18,7 +18,8 @@ export function addControlLayers(layers, directory, groupOrGroups) {
       id,
       name: symbol + label,
       group: groupOrGroups == 'group' ? group : subGroup,
-      directory
+      directory,  
+      exclusiveWithinGroup: layer.exclusiveWithinGroup
     };
   })
     .reverse()
