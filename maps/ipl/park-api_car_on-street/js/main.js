ba0201f4632801c3e3292b83f7b4c6f99f3f3441
Richard Forster <richard.forster@nvbw.de>
2025-06-24 17:53:08 +0200

    fix viewport

--- maps/ipl/park-api_car_on-street/js/main.js
+++ maps/ipl/park-api_car_on-street/js/main.js
@@ -4,7 +4,7 @@ import maplibregl from 'maplibre-gl';
 import '../css/styles.css';
 
 import {
-    /*map, */shape, fillShape, lineShape, maplibreControls, geocoder,
+    map, shape, fillShape, lineShape, maplibreControls, geocoder,
     addSources, addLayers,
     basemaps,
     popups
@@ -21,46 +21,14 @@ window.addEventListener('DOMContentLoaded', () => {
     // ==============================
     // MAP CONTROLS
     // ==============================  
-    // basemaps(map);
-
-    const CENTER = [9.0, 48.68];
-    const ZOOM   = window.innerWidth < 577 ? 6 : 7.1;
-    const BOUNDS = [
-      [5.8,  47.2],   // Southwest: [lon, lat]
-      [15.2, 55.1]    // Northeast: [lon, lat]
-    ];
-  
-    const map = new maplibregl.Map({
-      container: 'map',
-      style:     'https://tiles.mobidata-bw.de/styles/streets/style.json',
-      center:    CENTER,       // initial, kann überschrieben werden
-      zoom:      ZOOM,         // initial
-      minZoom:   4,
-      maxBounds: BOUNDS,       // initial, aber nur im Konstruktor evtl. nicht früh genug
-      attributionControl: false
-    });
-
-    function resetView() {
-        map.setCenter(CENTER);
-        map.setZoom(ZOOM);
-        map.setMaxBounds(BOUNDS);
-      }
-    
-      // 2) jedes Mal, wenn der Style (wieder) geladen ist
-      map.on('style.load', resetView);
-    
-      // 3) falls Style schon vor Listener-Registrierung geladen ist
-      if (map.isStyleLoaded && map.isStyleLoaded()) {
-        resetView();
-      }
-
-          geocoder(map);
+    basemaps(map);
+    geocoder(map);
     maplibreControls(map);
 
 
     // ==============================
     // SOURCES AND LAYERS
-    // ==============================   
+    // ==============================
     map.on('load', () => {
 
         // DEFAULT LAYERS
