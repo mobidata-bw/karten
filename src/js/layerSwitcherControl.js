844dcf76a39dea712b16a0154719745764faf3b5
Richard Forster <richard.forster@nvbw.de>
2025-09-26 15:58:54 +0200

    update basemaps

--- src/js/layerSwitcherControl.js
+++ src/js/layerSwitcherControl.js
@@ -14,7 +14,7 @@ export function basemaps(map, options = {}) {
     // ==============================
     // BASEMAPS & ATTRIBUTIONS
     // ==============================    
-    const mapLibre = "<a href='https://maplibre.org/' target='_blank'>MapLibre</a>";
+    const maplibre = "<a href='https://maplibre.org/' target='_blank'>MapLibre</a>";
     const openStreetMap = "<a href='https://www.openstreetmap.org/copyright' target='_blank'>© OpenStreetMap Mitwirkende</a>";
     const mapTiler = "<a href='https://www.maptiler.com/copyright/' target='_blank'>© MapTiler</a>";
     const lgl = "<a href='https://www.lgl-bw.de/' target='_blank'>© LGL-BW</a>";
@@ -24,37 +24,37 @@ export function basemaps(map, options = {}) {
             img: `${basePath}img/basemaps/streets.png`,
             style: 'https://tiles.mobidata-bw.de/styles/streets/style.json',
             title: 'Straßen',
-            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${mapLibre} | ${openStreetMap} ${mapTiler} </div>`
+            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${maplibre} | ${openStreetMap} ${mapTiler} </div>`
         },
         'bicycle': {
             img: `${basePath}img/basemaps/bicycle.png`,
             style: 'https://tiles.mobidata-bw.de/styles/bicycle/style.json',
             title: 'Fahrrad',
-            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${mapLibre} | ${openStreetMap} ${mapTiler} </div>`
+            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${maplibre} | ${openStreetMap} ${mapTiler} </div>`
         },
-         'railway': {
+        'railway': {
             img: `${basePath}img/basemaps/railway.png`,
             style: 'https://tiles.mobidata-bw.de/styles/railway/style.json',
             title: 'Schienen',
-            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${mapLibre} | ${openStreetMap} ${mapTiler} </div>`
-        },        
+            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${maplibre} | ${openStreetMap} ${mapTiler} </div>`
+        },
         'aerialphotos': {
             img: `${basePath}img/basemaps/aerialphotos.png`,
             style: 'https://tiles.mobidata-bw.de/styles/aerialphotos/style.json',
             title: 'Luftbild',
-            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${mapLibre} | ${openStreetMap} ${mapTiler} ${lgl} </div>`
+            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${maplibre} | ${openStreetMap} ${mapTiler} ${lgl} </div>`
         },
         'terrain': {
             img: `${basePath}img/basemaps/terrain.png`,
             style: 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_top.json',
-            title: 'Geländemodell',
-            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${mapLibre} </div>`
+            title: 'Gelände',
+            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${maplibre} </div>`
         },
         'darkmatter': {
             img: `${basePath}img/basemaps/darkmatter.png`,
             style: 'https://tiles.mobidata-bw.de/styles/darkmatter/style.json',
-            title: 'Dunkelmodus',
-            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${mapLibre} | ${openStreetMap} ${mapTiler} </div>`
+            title: 'Nacht',
+            attribution: `<div class='maplibregl-ctrl-attrib-inner'> ${maplibre} | ${openStreetMap} ${mapTiler} </div>`
         }
     };
 
@@ -107,11 +107,22 @@ export function basemaps(map, options = {}) {
             const basemaps = this._options.basemaps;
             Object.keys(basemaps).forEach((layerId) => {
                 const base = basemaps[layerId];
-                const basemapContainer = document.createElement('img');
-                basemapContainer.src = base.img;
+
+                const basemapContainer = document.createElement('figure');
                 basemapContainer.classList.add('basemap');
                 basemapContainer.dataset.id = layerId;
-                basemapContainer.title = base.title;
+                // basemapContainer.title = base.title;
+
+                const img = document.createElement('img');
+                img.classList.add('img');
+                img.src = base.img;
+
+                const figcaption = document.createElement('figcaption');
+                figcaption.classList.add('figcaption');
+                figcaption.innerText = base.title;
+
+                basemapContainer.append(img, figcaption);
+
 
                 basemapContainer.addEventListener('click', () => {
 
