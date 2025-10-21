ed7ff9896b4a28484150b31e118e7be8d3f4d4b0
Richard Forster <richard.forster@nvbw.de>
2025-10-21 11:57:31 +0200

    fix CSS for Google Street View

---
+++ dist_dev/assets/popupContent-C31KEYyZ.js
@@ -0,0 +1,29 @@
+import{s as r}from"./addControlLayers-PtuRffpj.js";const l=r({format:"mbtiles",file:"strassennetz"}),c=r({format:"mbtiles",file:"netzknoten"}),a={source:"sourceStrassennetz",sourceLayer:"strassennetz",type:"line",group:"Straßennetz und Netzknoten",subGroup:"Straßennetz",lineWidth:["interpolate",["linear"],["zoom"],6,2,12,3]},z=[{id:"strassennetz_K",label:"Kreisstraße",color:"#a88f00",filter:["==",["get","StrassenArt"],"K"],...a},{id:"strassennetz_L",label:"Landstraße",color:"#00ff00",filter:["==",["get","StrassenArt"],"L"],...a},{id:"strassennetz_B",label:"Bundesstraße",color:"#0089cd",filter:["==",["get","StrassenArt"],"B"],...a}],d=[{id:"netzknoten",label:"Netzknoten",group:"Straßennetz und Netzknoten",subGroup:"Netzknoten",source:"sourceNetzknoten",sourceLayer:"netzknoten",color:"#e12942",visibility:"none"}];function i(t){const{StrassenName:e,Anfangsnetzknoten:n,Endnetzknoten:s}=t;return`
+        <table>
+            <tr>
+                <th class="title">${e}</th>
+            </tr>
+        </table><br><table>
+            <tr>
+                <td class="att">Anfangsnetzknoten</td>
+                <td class="attContent">${n}</td>
+            </tr><tr>
+                <td class="att">Endnetzknoten</td>
+                <td class="attContent">${s}</td>
+            </tr>
+        </table>
+    `}function u(t){const{NK_Name:e,Netzknoten:n,NKArt:s}=t;return`
+        <table>
+            <tr>
+                <th class="title">${e}</th>
+            </tr>
+        </table><br><table>
+            <tr>
+                <td class="att">Netzknoten-ID</td>
+                <td class="attContent">${n}</td>
+            </tr><tr>
+                <td class="att">Netzknotenart</td>
+                <td class="attContent">${s}</td>
+            </tr>
+        </table>
+        `}export{l as a,z as b,u as c,d as l,i as p,c as s};
---
+++ dist_dev/assets/popupImages-D0t3k1hi.js
@@ -0,0 +1,24 @@
+import{p as t}from"./addControlLayers-PtuRffpj.js";function o(i){const s=`${t}img/popup/${i}`;return`
+    <th>
+      <img
+        src="${s}.svg"
+        alt="Logo von ${i}"
+        class="image"
+        style="visibility:hidden"
+        onload="this.style.visibility='visible'"
+        onerror="         
+          if (this.src.endsWith('.svg')) {
+            this.onerror = function() {             
+              this.onerror = null;
+              this.src = '${s}.jpg';
+            };
+            this.src = '${s}.png';
+          }         
+          else if (this.src.endsWith('.png')) {
+            this.onerror = null;
+            this.src = '${s}.jpg';
+          }
+        "
+      />
+    </th>
+  `}export{o as p};
